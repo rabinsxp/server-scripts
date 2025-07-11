04300fc4c0b6b4957722552f0d959694189b01a9
Rabins Sharma Lamichhane <rabinsrabins@gmail.com>
2025-07-11 15:15:41 +0100

    Update 04.sh

--- 04.sh
+++ 04.sh
@@ -3,5 +3,12 @@
 #Add the file 
 cp ifcfg-eth0 /etc/sysconfig/network-scripts/
 
-sudo systemctl restart network
+# sudo systemctl restart network // this has been depracated so use the following
+
+sudo systemctl restart NetworkManager
+
+#or the following
+
+nmcli connection reload
+nmcli connection up br0
 
