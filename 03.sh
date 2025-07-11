f16d01234549b59d56d69078accfc0d7c82666e2
Rabins Sharma Lamichhane <rabinsrabins@gmail.com>
2025-07-11 15:07:14 +0100

    Update 03.sh

--- 03.sh
+++ 03.sh
@@ -1,5 +1,5 @@
 cat /etc/sysconfig/network-scripts/ifcfg-br0
 
-cp /etc/sysconfig/network-scripts/ifcfg-br0to /etc/sysconfig/network-scripts/ifcfg-br0.bak
+cp /etc/sysconfig/network-scripts/ifcfg-br0 /etc/sysconfig/network-scripts/ifcfg-br0.bak
 
 cp ifcfg-br0 to /etc/sysconfig/network-scripts/ifcfg-br0
